c092ebc8b501b078c1667d627e0508af9a35b440
Gabriel Pereira <gabrielpereira@MacBook-Air-de-Gabriel.local>
2023-05-06 21:41:06 -0300

    feat: endpoint de login

--- src/index.js
+++ src/index.js
@@ -7,4 +7,22 @@ app.get("/", (req, res) => {
     res.send({message: "Api ok"})
 })
 
+app.get("/clientes", (req, res) => {
+    const customers = [
+        { id:3, name: "Souza"},
+        { id:7, name: "Edilson"}
+    ]
+    res.status(200).send(customers);
+})
+
+
+app.post("/login", (req, res) => {
+    const { user, password } = req.body;
+    if(user === "Souza" && password === "92024080"){
+        return res.end();
+    }
+
+    return res.status(401).end();
+})
+
 app.listen(3001, () => console.log("Server Running 3001"))
